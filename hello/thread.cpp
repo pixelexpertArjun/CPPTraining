b415281d35f7acc6e287d0f5dba9d4f8bf39364f
pixelexpertArjun <arjun.r@pixelexpert.net>
2025-02-10 19:25:37 +0530

    thread_2

--- hello/thread.cpp
+++ hello/thread.cpp
@@ -5,45 +5,57 @@
 #include <mutex>
 #include <thread>
 
-std::mutex m1;
-std::condition_variable m1cond;
-int i = 0;
-bool produced = false;
-bool consumed = false;
+std::mutex engine_mtx;
+std::condition_variable engine_cnd;
+int ign = 2; // int ign = 1;
+int cluster_init = 0;
+int info_init = 0;
 
-void process1(std::shared_ptr<int> sharedptrp1) {
-  for (int j = 0; j < 11; j++) {
-    std::unique_lock m1unq(m1);
-    m1cond.wait(m1unq, [] { return !produced; });
-    i++;
-    std::cout << "process 1 : " << i << std::endl;
-    produced = true;
-    m1unq.unlock();
-    m1cond.notify_one();
+void engine_ign() {
+  std::cout << "Engine : waiting for ignition..." << std::endl;
+  for (int j = 1; j < 11; j++) {
+    std::unique_lock<std::mutex> engine_ul(engine_mtx);
+    engine_cnd.wait(engine_ul, [] { return ign == 2; });
+    std::cout << "Engine ignition started : " << j << std::endl;
+    ign = 0;
+    cluster_init = 1;
+    engine_ul.unlock();
+    engine_cnd.notify_all();
   }
 }
 
-void process2(std::shared_ptr<int> sharedptrp2) {
-  for (int j = 0; j < 11; j++) {
-    std::unique_lock m1unq(m1);
-    m1cond.wait(m1unq, [] { return produced; });
-    i--;
-    std::cout << "process 2 : " << i << std::endl;
-    produced = false;
-    m1unq.unlock();
-    m1cond.notify_one();
+void cluster() {
+  for (int j = 1; j < 11; j++) {
+    std::unique_lock<std::mutex> cluster_ul(engine_mtx);
+    engine_cnd.wait(cluster_ul, [] { return cluster_init == 1; });
+    std::cout << "Cluster turned on... " << j << std::endl;
+    info_init = 1;
+    cluster_init = 0;
+    cluster_ul.unlock();
+    engine_cnd.notify_all();
+  }
+}
+
+void infotaiment() {
+  for (int j = 1; j < 11; j++) {
+    std::unique_lock<std::mutex> info_ul(engine_mtx);
+    engine_cnd.wait(info_ul, [] { return info_init == 1; });
+    std::cout << "infotaiment turned on... " << j << std::endl;
+    ign = 1;
+    info_init = 0;
+    info_ul.unlock();
+    engine_cnd.notify_all();
   }
 }
 
 int main() {
-  std::shared_ptr<int> sharedptr(new int{10});
-
-  std::thread t1(process1, sharedptr);
-  std::thread t2(process2, sharedptr);
-  // std::thread t3(process3 );
+  // ign = 1;
+  std::thread t1(engine_ign);
+  std::thread t2(cluster);
+  std::thread t3(infotaiment);
 
   t1.join();
   t2.join();
-
-  std::cout << "exited!!" << std::endl;
+  t3.join();
+  std::cout << "Loop ended!!" << std::endl;
 }
